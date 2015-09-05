99f5ce6a1d341c6a2626cf569d27816f4e06f227
TheFastbreak <michael.e.davisjr@gmail.com>
2015-09-05 05:34:38 -0400

    add static files

---
+++ static/js/controller/main.js
@@ -0,0 +1,23 @@
+var app = angular.module('treatment', []);
+app.controller('results', ['$scope', '$http', function($scope, $http) {
+	$scope.fullScreen = false;
+
+	$scope.submit = function() {
+		if ($scope.text) {
+			$http.get('/search?q='+$scope.text).success(function(response) {
+				$scope.results = response
+			})
+		}
+	}
+
+	$scope.blur = function ($event) {
+		if ($scope.text) return;
+		$scope.fullScreen = false;
+	}
+
+	$scope.focus = function ($event) {
+		$scope.fullScreen = true;
+	}
+
+
+}])
