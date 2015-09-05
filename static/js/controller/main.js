bbf019c383af0540cf2b7575b7f999395bc33199
root <root@debian>
2015-09-05 10:21:42 +0000

    Fix screen width

--- static/js/controller/main.js
+++ static/js/controller/main.js
@@ -21,7 +21,7 @@ app.controller('results', ['$scope', '$http', function($scope, $http) {
 
 	$scope.focus = function ($event) {
 		$scope.searching = true;
-		$('#search_results').height(window.innerHeight - $('header').height() - $('fieldset').outerHeight(true) - 20)
+		$('#search_results').height(window.innerHeight - $('header').height() - $('fieldset').outerHeight(true) - 25)
 		$('#search_results').slideDown()
 	}
 }])
