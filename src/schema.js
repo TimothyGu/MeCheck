8cde86009c1f4c04f9f46a4d75e52bfe7999d480
Dajour <dajour.nash01@gmail.com>
2015-09-05 11:00:26 -0400

    add additional schema

--- src/schema.js
+++ src/schema.js
@@ -2,6 +2,15 @@ var mongoose = require('mongoose')
 
 var Schema   = mongoose.Schema,
     ObjectId = Schema.ObjectId
+
+var Log_User_Account = new Schema({
+  id: ObjectId,
+  search_value: String,
+  lat: String,
+  lon: String
+})
+
+module.exports = mongoose.model('Insert', Insert)
  
 var Test = new Schema({
   id: ObjectId,
