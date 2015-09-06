07160153362c335dcf15ce8fad1f65824a3702eb
Dajour <dajour.nash01@gmail.com>
2015-09-05 22:00:41 -0400

    add all update files

--- static/js/controller/main.js
+++ static/js/controller/main.js
@@ -4,6 +4,11 @@ app.controller('results', ['$scope', '$http', function($scope, $http) {
 	$scope.fullScreen = false;
 	$scope.searching = false;
 
+	$http.get('https://data.phila.gov/resource/9hed-4ffe.json').success(function(response) {
+		console.log('lat: '+response[0].location_1.latitude)
+		console.log('lon: '+response[0].location_1.longitude)
+	})
+
 	$scope.load = function() {
 	 	if ($scope.text) {
 			$http.get('/search?q='+$scope.text).success(function(response) {
